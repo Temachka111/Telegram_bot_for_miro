f477819b881d2fa124ad6e3141970efcc465492c
Temachka111 <Artemka353535@yandex.ru>
2026-02-20 15:10:17 +0300

    init commit

---
+++ src/main/java/com/example/Telegram_bot_for_Miro/TelegramBotForMiroApplication.java
@@ -0,0 +1,13 @@
+package com.example.Telegram_bot_for_Miro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotForMiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotForMiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Telegram_bot_for_Miro/TelegramBotForMiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Telegram_bot_for_Miro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotForMiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
